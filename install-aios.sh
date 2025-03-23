4797444b74fcf8f767bb0ef2c17237b99d7d6ea9
airdropzenith <airdrozenith@gmail.com>
2025-03-24 00:47:50 +0200

    Update install-aios.sh

--- install-aios.sh
+++ install-aios.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Define colors
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-# Print the header section with ASCII art
-echo -e "${GREEN}# Header Section${NC}"
-echo -e "${GREEN}Cminerbox${NC}"
-echo -e "${GREEN} _____           _                 _               ${NC}"
-echo -e "${GREEN}/  __ \         (_)               | |              ${NC}"
-echo -e "${GREEN}| /  \/_ __ ___  _ _ __   ___ _ __| |__   _____  __${NC}"
-echo -e "${GREEN}| |   | '_ ` _ \| | '_ \ / _ \ '__| '_ \ / _ \ \/ /${NC}"
-echo -e "${GREEN}| \__/\ | | | | | | | | |  __/ |  | |_) | (_) >  < ${NC}"
-echo -e "${GREEN} \____/_| |_| |_|_|_| |_|\___|_|  |_.__/ \___/_/\_\\${NC}"
+# Header section 
+echo "  _____           _                 _               "
+echo " /  __ \         (_)               | |              "
+echo " | /  \/_ __ ___  _ _ __   ___ _ __| |__   _____  __"
+echo " | |   | '_ ` _ \| | '_ \ / _ \ '__| '_ \ / _ \ \/ /"
+echo "| \__/\ | | | | | | | | |  __/ |  | |_) | (_) >  < "
+echo " \____/_| |_| |_|_|_| |_|\___|_|  |_.__/ \___/_/\_\\"
                                                                          
-
 # Display Twitter and Telegram links
 echo "🔹 Follow us on Twitter: @airdropzenith_"
 echo "🔹 Join our Telegram Channel: https://t.me/airdropzenith"
